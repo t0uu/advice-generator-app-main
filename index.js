f081249729a943c8658fe846c8821025f856bedf
t0uu <srtouzet@hotmail.com>
2022-04-02 14:26:32 -0300

    add animation

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ const Btn = document.getElementById('Btn')
 
 
 
-gsap.to("#Btn", {rotation: 360, duration: 1.1})
 
+gsap.to("#Btn", {rotation: 360, duration: 1.1}) 
 
 
 Btn.addEventListener('click', async() => {
+ 
    try {
     const res = await fetch('https://api.adviceslip.com/advice')
     const data = await res.json()
-    
+    gsap.fromTo("#Number", {opacity: 0, duration: 2}, {opacity:1})
+    gsap.fromTo("#Advice", {opacity: 0, duration: 2}, {opacity:1})
     Number.innerText = `ADVICE #${data.slip.id}`
     Advice.innerText = `"${data.slip.advice}"`  
    } catch{
       console.error(error);
     }
 
-    
    } 
     );
 
